22f4bc49f9942cd74bc6588f165c09e23a836b3b
Brendan Willis <vuulgen2@gmail.com>
2022-03-19 22:16:30 -0600

    up to snuff

--- GitHub/week9-end/teach_solution.py
+++ GitHub/week9-end/teach_solution.py
@@ -1,6 +1,6 @@
-import csv
+# Copyright 2020, Brigham Young University-Idaho. All rights reserved.
 
-from add_area_code import main
+import csv
 
 
 # Each row in the pupils.csv file contains three elements.
@@ -9,17 +9,29 @@ GIVEN_NAME_INDEX = 0
 SURNAME_INDEX = 1
 BIRTHDATE_INDEX = 2
 
+
 def main():
     try:
-        students_list = read_compound_list('pupils.csv')
+        # Read the pupils.csv file into a compound list.
+        students_list = read_compound_list("pupils.csv")
 
-        extract_birthday = lambda student: student[BIRTHDATE_INDEX]
+        # As a debugging aid, print the students_list.
+        #print(students_list)
 
-        sorted_list = sorted(students_list, key=extract_birthday)
-        print(sorted_list)
+        # Define a lambda function that extracts a student's birthdate.
+        extract_birthdate = lambda student: student[BIRTHDATE_INDEX]
+
+        # Call the Python built-in sorted function to sort
+        # the list of students by their birthdate and pass
+        # the lambda function to the sorted function.
+        sorted_list = sorted(students_list, key=extract_birthdate)
+
+        # Call the print_list function to print the sorted list.
+        print_list(sorted_list)
 
     except (FileNotFoundError, PermissionError) as error:
-        print(type(error).__name__, error, sep=': ')
+        print(type(error).__name__, error, sep=": ")
+
 
 def read_compound_list(filename):
     """Read the text from a CSV file into a compound list.
@@ -53,11 +65,23 @@ def read_compound_list(filename):
 
     return compound_list
 
+
 def print_list(compound_list):
-    for student in compound_list:
-        print(student)
+    """Print the elements in a compound list to
+    the terminal window for the user to see.
+
+    Parameter
+        compound_list: a list that contains other lists
+    Return: nothing
+    """
+    for element in compound_list:
+        print(element)
     print()
 
-if __name__ == '__main__':
+
+# If this file is executed like this:
+# > python teach_solution.py
+# then call the main function. However, if this file is simply
+# imported (e.g. into a test file), then skip the call to main.
+if __name__ == "__main__":
     main()
-        
